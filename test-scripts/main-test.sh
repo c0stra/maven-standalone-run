7051cc603f5383f7b84c29e180208ee29c999d4f
c0stra <ondrej.fischer@gmail.com>
2021-11-02 10:14:25 +0100

    Introduced test scripts for testing of the plugin.

---
+++ test-scripts/main-test.sh
@@ -0,0 +1,7 @@
+set -x
+cd ../run
+mvn clean install
+cd ../sample-main-artifact
+mvn clean install
+cd ..
+mvn -X foundation.fluent.api:run:main -Dartifact=foundation.fluent.api:sample-main-artifact:1.0-SNAPSHOT -DallowSnapshot=true
---
+++ test-scripts/testng-test.sh
@@ -0,0 +1,7 @@
+set -x
+cd ../run
+mvn clean install
+cd ../sample-testng-artifact
+mvn clean install
+cd ..
+mvn -X foundation.fluent.api:run:testng -Dartifact=foundation.fluent.api:sample-testng-artifact:1.0-SNAPSHOT -DallowSnapshot=true
